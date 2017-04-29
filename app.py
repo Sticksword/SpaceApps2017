99f5088485ba9454aa8d3fe3d49a2483d290801e
Michael Chen <mzc2fd@virginia.edu>
2017-04-29 15:16:27 -0700

    adding sqlite support and initial schema script

--- app.py
+++ app.py
@@ -1,18 +1,58 @@
 #!/usr/bin/env python
 
-from flask import Flask, redirect, url_for, render_template, jsonify
+from flask import Flask, redirect, url_for, render_template, jsonify, g
 from pyproj import Proj, transform
-import requests
+import sqlite3, csv, requests
 
 # Flask app should start in global layout
 app = Flask(__name__, static_url_path='')
 wgs84 = Proj(init='EPSG:4326')
 
+# db stuff
 
+DATABASE = 'spaceapps.db'
+
+def init_db():
+    with app.app_context():
+        db = get_db()
+        with app.open_resource('init.sql', mode='r') as f:
+            db.cursor().executescript(f.read())
+
+        with open('crops.csv','rb') as fin: # `with` statement available in 2.5+
+            # csv.DictReader uses first line in file for column headings by default
+            datarow = csv.DictReader(fin) # comma is default delimiter
+            to_db = [(i['longitude'], i['latitude'], i['type']) for i in datarow]
+
+        db.cursor().executemany("INSERT INTO CropData (longitude, latitude, type) VALUES (?, ?, ?);", to_db)
+        db.commit()
+
+def get_db():
+    db = getattr(g, '_database', None)
+    if db is None:
+        db = g._database = sqlite3.connect(DATABASE)
+    return db
+
+@app.teardown_appcontext
+def close_connection(exception):
+    db = getattr(g, '_database', None)
+    if db is not None:
+        db.close()
+
+def query_db(query, args=(), one=False):
+    cur = get_db().execute(query, args)
+    rv = cur.fetchall()
+    cur.close()
+    return (rv[0] if rv else None) if one else rv
+
+# routes
 
 @app.route('/')
 def index():
     return render_template('index.html')
+
+@app.route('/anothertest')
+def wheee():
+    return jsonify(query_db('SELECT * FROM CropData'))
 
 @app.route('/test')
 def convert():
@@ -29,6 +69,7 @@ def convert():
 if __name__ == '__main__':
     port = 5000
     print('Starting app on port %d' % port)
+    init_db()
     app.run(debug=True, port=port, host='0.0.0.0')
     url_for('static', filename='app.js')
     url_for('static', filename='app.css')
--- testing_stuff.py
+++ testing_stuff.py
@@ -1,15 +1,28 @@
-from pyproj import Proj, transform
+# from pyproj import Proj, transform
+#
+# inProj = Proj(init='epsg:3857')
+# outProj = Proj(init='epsg:4326')
+# x1,y1 = -11705274.6374,4826473.6922
+# x2,y2 = transform(inProj,outProj,x1,y1)
+# print x2,y2
+#
+#
+# wgs84 = Proj(init='EPSG:4326')
+# lat = 50
+# lon = 50
+# x, y = wgs84(lon, lat)
+#
+# print x, y
+import requests
 
-inProj = Proj(init='epsg:3857')
-outProj = Proj(init='epsg:4326')
-x1,y1 = -11705274.6374,4826473.6922
-x2,y2 = transform(inProj,outProj,x1,y1)
-print x2,y2
+url = 'https://nassgeodata.gmu.edu/axis2/services/CDLService/GetCDLValue'
 
+querystring = {'year':'2010','x':'1551459.363','y':'1909201.537'}
 
-wgs84 = Proj(init='EPSG:4326')
-lat = 50
-lon = 50
-x, y = wgs84(lon, lat)
+response = requests.request('GET', url, params=querystring, verify=False)
 
-print x, y
+print(response.text)
+# from bs4 import BeautifulSoup
+# soup = BeautifulSoup(response.text, 'xml')
+#
+# print soup.result.string
